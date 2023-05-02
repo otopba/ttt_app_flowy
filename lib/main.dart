1758690b9812180f64e7ac455966184963591f74
otopba <otopba@inbox.ru>
2023-05-02 17:40:21 +0700

    lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ttt_app_flowy/app.dart';
 import 'package:ttt_app_flowy/di.dart';
 import 'package:ttt_app_flowy/log.dart';
@@ -8,6 +9,10 @@ import 'package:ttt_app_flowy/log.dart';
 const _tag = 'main';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   di.setup();
 
   runZonedGuarded<Future<void>>(
